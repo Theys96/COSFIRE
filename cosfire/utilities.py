b568b5ab4eee747a7ab3dc57b763cac0927199cc
Theys96 <accounts@thijshavinga.nl>
2018-04-19 16:26:24 +0200

    Created ImageStack to keep track of images with their respective filter parameters

--- cosfire/utilities.py
+++
@@ -1,9 +0,0 @@
-import cosfire
-
-class ImageStack():
-
-    def __init__(self, filter, filterArgs):
-        for arg in filterArgs:
-            print(arg)
-
-stack = ImageStack(cosfire.DoGFilter, (2.6, 1))
--- examples/libtest.py
+++ examples/libtest.py
@@ -6,17 +6,21 @@ import matplotlib.pyplot as plt
 
 
 # Prototype image
-proto = np.asarray(Image.open('prototype4.png').convert('L'), dtype=np.float64)
-(cx, cy) = (50,50)
+proto = np.asarray(Image.open('tomato.jpg').convert('L'), dtype=np.float64)
+(cx, cy) = (305,305)
 
 # Create COSFIRE operator and fit it with the prototype
 cosf = c.COSFIRE(
-		c.CircleStrategy, c.DoGFilter, (2.6, 1), [0,20,40]
+		c.CircleStrategy, c.DoGFilter, (2.6, 1), [x*5 for x in range(0,60)]
 	   ).fit(proto, (cx, cy))
 
 # Draw filtered prototype
 tupleCoords = [( cx+int(round(rho*m.sin(phi))) , cy+int(round(rho*m.cos(phi))) ) for (sigma, rho, phi) in cosf.strategy.tuples]
+print(tupleCoords);
 plt.imshow(cosf.strategy.filteredProto, cmap='gray')
 tupleCoords = np.asarray(tupleCoords)
-plt.plot(tupleCoords[:,1], tupleCoords[:,0],"xr")
+try:
+	plt.plot(tupleCoords[:,1], tupleCoords[:,0],"xr")
+except:
+	print("No tuples found")
 plt.show()
