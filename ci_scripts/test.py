ae06e8fb477a18a34b28b6cd3bab42624a8db462
Theys96 <accounts@thijshavinga.nl>
2018-04-23 22:22:06 +0200

    Moved picture

--- ci_scripts/test.py
+++ ci_scripts/test.py
@@ -12,7 +12,7 @@ class TestMethods(unittest.TestCase):
         from PIL import Image
 
         # Prototype image
-        proto = np.asarray(Image.open('prototype.png').convert('L'), dtype=np.float64)
+        proto = np.asarray(Image.open('examples/prototype1.png').convert('L'), dtype=np.float64)
         cx, cy = (50,50)
 
         # Create COSFIRE operator and fit it with the prototype
